240ce096d5674539314c76b37031975d9c00a1be
Kate G <h34rt8e4t@gmail.com>
2022-10-09 17:17:09 -0700

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Kate G on 10/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
